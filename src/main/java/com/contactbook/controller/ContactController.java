959fc39a82293c6eec794fcddf978f4cfdde949b
Gabriela Dantas <gabriela.dantas@inovafarma.com.br>
2022-08-23 20:00:57 -0300

    Commit 01

--- src/main/java/com/contactbook/controller/ContactController.java
+++ src/main/java/com/contactbook/controller/ContactController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/contact")
 @CrossOrigin("*")
@@ -21,4 +23,15 @@ public class ContactController {
         return repository.save(contact);
     }
 
+    @GetMapping(value = "/list")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Contact> list() {
+        return repository.findAll();
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
 }
