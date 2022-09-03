fb31095c74087e35e8bc5ef6abbbc2ed2b72bb2c
Gabriela Dantas <gabriela.dantas@inovafarma.com.br>
2022-09-03 13:59:48 -0300

    Corrige arquivo securityconfig

--- src/main/java/com/contactbook/configuration/SecurityConfig.java
+++ src/main/java/com/contactbook/configuration/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SuppressWarnings("deprecation")
@@ -41,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder() {
+    public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
 }
